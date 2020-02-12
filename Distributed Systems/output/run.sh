c7efdec7b435b7c36a89c9cd61723d0e9ee43841
Wei Loon <47574881+cwlroda@users.noreply.github.com>
2020-02-12 03:54:48 +0000

    Updates

--- Distributed Systems/output/run.sh
+++ Distributed Systems/output/run.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
-INPUT=test.csv
+INPUT=data/test.csv
 IFS=","
 
 [ ! -f $INPUT ] && { echo "$INPUT file not found!"; exit 99; }
 while read field1 field2 field3
 do
-    ./screen_dump $field1 $field2 $field3;
+    ./output/screen_dump $field1 $field2 $field3;
     echo ""
 done < $INPUT
